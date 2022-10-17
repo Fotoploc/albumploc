7705884edd0214873216bd9a33cb2af00e85a97c
NondasNF <nondasnoronha@gmail.com>
2022-10-17 08:03:33 -0300

    update migration and schema

--- db/migrate/20221014140013_create_users_albums_join_table.rb
+++ db/migrate/20221014140013_create_users_albums_join_table.rb
@@ -1,6 +1,6 @@
 class CreateUsersAlbumsJoinTable < ActiveRecord::Migration[7.0]
   def change
-    create_join_table :categories, :users do |t|
+    create_join_table :albums, :users do |t|
       t.index :user_id
       t.index :album_id
     end
--- db/migrate/20221014140410_create_users_photos_join_table.rb
+++ db/migrate/20221014140410_create_users_photos_join_table.rb
@@ -1,6 +1,6 @@
 class CreateUsersPhotosJoinTable < ActiveRecord::Migration[7.0]
   def change
-    create_join_table :categories, :users do |t|
+    create_join_table :photos, :users do |t|
       t.index :user_id
       t.index :photo_id
     end
