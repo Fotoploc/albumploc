ba72b004e374375fc37a9fa54e46ce13df0db62c
NondasNF <nondasnoronha@gmail.com>
2022-10-21 08:42:34 -0300

    create, config profile page and it route

---
+++ app/controllers/users/user_controller.rb
@@ -0,0 +1,12 @@
+class Users::UserController < ApplicationController
+  before_action :authenticate_user!, only: [:profile]
+
+  def all
+    @users = User.all
+  end
+
+  def profile
+    @user = User.find(current_user.id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   resources :photos
   resources :albums
   devise_for :views
+  get 'user/profile' => 'users/user#profile'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
