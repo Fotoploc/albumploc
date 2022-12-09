e04e767493fb9da4f2ff0bb5263f0d88919e2190
NondasNF <nondasnoronha@gmail.com>
2022-12-09 08:42:07 -0300

    Adicionar pagina ao album

---
+++ app/controllers/album_pages_controller.rb
@@ -0,0 +1,45 @@
+class AlbumPagesController < ApplicationController
+  before_action :authenticate_user!
+  load_and_authorize_resource
+
+  def show
+    @album = Album.find(params[:id])
+    @album_pages = @album.album_page.all
+  end
+
+  def edit_pages
+    @album = Album.find(params[:album_id])
+    @album_pages = @album.album_page.all
+    @album_page_new = AlbumPage.new
+  end
+  
+  def create
+    @album_page = AlbumPage.new(album_page_params)
+    if @album_page.save
+      redirect_to edit_album_pages_path(@album_page.album_id)
+    else
+
+    end
+  end
+
+  def update
+    @album_page = AlbumPage.find(params[:id])
+    if @album_page.update(album_page_params)
+      redirect_to edit_album_pages_path(@album_page.album_id)
+    else
+      render edit_album_pages_path(@album_page.album_id), @album_page_new.erros = "Error"
+    end
+  end
+
+  def destroy
+    @album_page = AlbumPage.find(params[:id])
+    @album_page.destroy
+    redirect_to edit_album_pages_path(@album_page.album_id)
+  end
+
+  private
+  def album_page_params
+    params.require(:album_page).permit(:album_id, :page_number, :background_image)
+  end
+
+end
--- app/models/album_page.rb
+++ app/models/album_page.rb
@@ -1,4 +1,6 @@
 class AlbumPage < ApplicationRecord
   belongs_to :album
   has_many :stickers, class_name: 'PageSticker', foreign_key: 'album_page_id'
+
+  validates :page_number, presence: true, uniqueness: true
 end
