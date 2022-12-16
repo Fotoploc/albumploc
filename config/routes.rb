20197b981a2a75b2f7c00d9404727ea620cee21b
NondasNF <nondasnoronha@gmail.com>
2022-12-16 07:16:01 -0300

    create show exchange page

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   get 'user/:user_id/albums/:album_id' => 'albums#specific_user_album', as: 'specific_user_album'
   get 'user/:user_id/albums/:album_id/exchange' => 'exchange#index', as: 'exchange'
   post 'user/:user_id/albums/:album_id/exchange' => 'exchange#new', as: 'exchange_new'
+  get 'user/exchanges/:exchange_id' => 'exchange#show', as: 'exchange_show'
   get 'user/exchanges' => 'exchange#all', as: 'all_exchanges'
   put 'user/exchanges/:exchange_id/cancel' => 'exchange#cancel', as: 'cancel_exchange'
   put 'user/exchanges/:exchange_id/refuse' => 'exchange#refuse', as: 'refuse_exchange'
